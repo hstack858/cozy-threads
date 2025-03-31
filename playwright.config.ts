65a1a6c95de564458b6e852376b6a40f56e8f9b5
Henry Stachowiak <stachowiak.h@northeastern.edu>
2025-03-31 11:41:55 -0500

    Fixing Server deploy

--- playwright.config.ts
+++ playwright.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from '@playwright/test';
 
 export default defineConfig({
   testDir: './spec',
+  timeout: 120 * 1000,
   use: {
     baseURL: 'http://localhost:3000',
     headless: true,
@@ -10,9 +11,10 @@ export default defineConfig({
     video: 'retain-on-failure',
   },
   webServer: {
-    command: 'npx concurrently "npm run start --workspace=apps/server" "npm run start --workspace=apps/client"',
+    command:
+      'npx concurrently "npm run start --workspace=apps/server" "npm run start --workspace=apps/client"',
     port: 3000,
-    timeout: 60 * 1000,
+    timeout: 120 * 1000,
     reuseExistingServer: !process.env.CI,
   },
 });
